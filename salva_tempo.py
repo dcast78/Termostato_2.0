1663b13d8637a69893a9380e2adf6d7f4e637eaf
Daniele <dcast78@gmail.com>
2016-02-04 17:23:03 +0100

    rimosso rpush rele perchè variabile già inserita dalla lista camere

--- salva_tempo.py
+++ salva_tempo.py
@@ -18,7 +18,6 @@ else:
 
 r = redis.StrictRedis(host=db_host, port=6379, db=db_id,password='Termostato_2.0')
 r.rpush("timestamp",time.strftime("%s"))
-r.rpush("rele","")
 #r.rpush("setpoint","")
 i=0
 while i < r.llen("camere"):
