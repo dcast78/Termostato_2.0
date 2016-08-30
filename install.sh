f3f3d171d575e587c04ddf47d384b0a7fdac1f56
dcast78 <dcast78@gmail.com>
2016-08-30 13:10:17 +0200

    Generazione comandi da inserire in crontab
    Aggiunta comando per scansione sensori direttamente collegati a bus 1 wire.
    Semplifica la creazione della riga per utenti.

--- install.sh
+++ install.sh
@@ -14,3 +14,9 @@ do
 done
 
 ln -s /usr/termostato/etc/sudoers.d/grav /etc/sudoers.d/grav
+
+echo "Ricerca sensori collegati al bus 1Wire"
+ls -l /sys/bus/w1/devices/
+
+echo "Ora viene generata una riga da inserire su crontab di root, devi sostituire nome_sensore con una stringa che identifica ad esempio una stanza. Se i sensori vengono usati per piu' istanza devi anche sostituire il valore 0 con il numero di istanza appropriato. Per i dettagli vedi http://www.raspibo.org/wiki/index.php?title=Termostato_2.0"
+echo -n "*/5 * * * * /root/termostato/salva_tempo.py termostato_m 0; " ; for id_sensore in `ls -1| grep -v w1_bus_master1` ; do  echo -n "/root/termostato/salva_temp.py `hostname` 0 nome_sensore $id_sensore;" ; done; echo " /root/termostato/termostato.py termostato_m 0 > /dev/null"
