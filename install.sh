b699443c46d79c71b1fe9bc3fcaa28dfb1d99a7c
daniele <dcast78@gmail.com>
2016-08-30 13:21:13 +0200

    Inserite indicazioni per aggiunta token bot telegram

--- install.sh
+++ install.sh
@@ -20,3 +20,7 @@ ls -l /sys/bus/w1/devices/
 
 echo "Ora viene generata una riga da inserire su crontab di root, devi sostituire nome_sensore con una stringa che identifica ad esempio una stanza. Se i sensori vengono usati per piu' istanza devi anche sostituire il valore 0 con il numero di istanza appropriato. Per i dettagli vedi http://www.raspibo.org/wiki/index.php?title=Termostato_2.0"
 echo -n "*/5 * * * * /root/termostato/salva_tempo.py termostato_m 0; " ; for id_sensore in `ls -1| grep -v w1_bus_master1` ; do  echo -n "/root/termostato/salva_temp.py `hostname` 0 nome_sensore $id_sensore;" ; done; echo " /root/termostato/termostato.py termostato_m 0 > /dev/null"
+
+echo "Ora per utilizzare il bot telegram devi ottenere un token, segui le indicazioni che trovi qui: https://core.telegram.org/bots."
+echo "Quando hai il token, inseriscilo nel file /home/pi/mytokens.py"
+
